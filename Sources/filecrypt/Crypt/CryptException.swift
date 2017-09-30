0e8a5de2ca24aa57c5c477e49cb61c493d0bb5a1
Ybrin <koray@koska.at>
2017-09-30 14:35:50 +0200

    Add error handling

--- Sources/filecrypt/Crypt/CryptException.swift
+++ Sources/filecrypt/Crypt/CryptException.swift
@@ -27,3 +27,42 @@ struct CryptException {
         case decryptionFailed(details: String)
     }
 }
+
+class CryptExceptionHandler {
+
+    static func handleIn(_ inError: CryptException.In, logger: Logger) {
+        let msg: String
+        switch inError {
+        case .fileDoesNotExist(let path):
+            msg = "The file \(path) does not exist."
+        case .cannotReadFile(let path):
+            msg = "Cannot read the file \(path)."
+        }
+
+        logger.error(msg)
+    }
+
+    static func handleOut(_ out: CryptException.Out, logger: Logger) {
+        let msg: String
+        switch out {
+        case .fileAlreadyExists(let path):
+            msg = "The file \(path) already exists."
+        case .cannotWriteFile(let path):
+            msg = "Cannot write the file \(path)."
+        }
+
+        logger.error(msg)
+    }
+
+    static func handleCrypt(_ crypt: CryptException.Crypt, logger: Logger) {
+        let msg: String
+        switch crypt {
+        case .encryptionFailed(let details):
+            msg = "Encryption failed. Details: \(details)"
+        case .decryptionFailed(let details):
+            msg = "Decryption failed. Details: \(details)"
+        }
+
+        logger.error(msg)
+    }
+}
--- Sources/filecrypt/main.swift
+++ Sources/filecrypt/main.swift
@@ -35,21 +35,35 @@ guard let filepath = filepath, encrypt ^^ decrypt else {
     exit(1)
 }
 
-if encrypt {
-    var pass: String
-    var validation: String
+do {
+    if encrypt {
+        var pass: String
+        var validation: String
 
-    (pass, validation) = CLI.getPasswordWithValidation()
-    while pass != validation {
-        logger.warn("Your passwords didn't match. Please try again.")
         (pass, validation) = CLI.getPasswordWithValidation()
+        while pass != validation {
+            logger.warn("Your passwords didn't match. Please try again.")
+            (pass, validation) = CLI.getPasswordWithValidation()
+        }
+
+        let c = try Encryptor(filepath: filepath, logger: logger)
+        try c.crypt(withPassword: pass)
+    } else if decrypt {
+        let pass = CLI.getPassword()
+
+        let c = try Decryptor(filepath: filepath, logger: logger)
+        try c.crypt(withPassword: pass)
+    }
+} catch {
+    if let e = error as? CryptException.In {
+        CryptExceptionHandler.handleIn(e, logger: logger)
+    } else if let e = error as? CryptException.Out {
+        CryptExceptionHandler.handleOut(e, logger: logger)
+    } else if let e = error as? CryptException.Crypt {
+        CryptExceptionHandler.handleCrypt(e, logger: logger)
+    } else {
+        logger.error("Unexpected error occurred. Consider opening an issue on Github.")
     }
 
-    let c = try Encryptor(filepath: filepath, logger: logger)
-    try c.crypt(withPassword: pass)
-} else if decrypt {
-    let pass = CLI.getPassword()
-
-    let c = try Decryptor(filepath: filepath, logger: logger)
-    try c.crypt(withPassword: pass)
+    exit(123)
 }
